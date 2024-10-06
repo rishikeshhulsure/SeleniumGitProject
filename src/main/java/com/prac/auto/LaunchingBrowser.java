5d44483bb6e39911cb763e2dceaedecf6065ecc1
rishikeshhulsure <rishikesh.hulsure@gmail.com>
2024-10-06 16:22:13 +0530

    made some changes in LaunchBrowser class

--- src/main/java/com/prac/auto/LaunchingBrowser.java
+++ src/main/java/com/prac/auto/LaunchingBrowser.java
@@ -1,5 +1,6 @@
 package com.prac.auto;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +8,7 @@ public class LaunchingBrowser {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
-		driver.findElement();
+		driver.findElement(By.cssSelector("textarea[class='gLFyf']")).sendKeys("Rishikesh Hulsure");
+		driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@value='Google Search']")).click();
 	}
 }
