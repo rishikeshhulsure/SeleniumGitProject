462531b399fc4d7c381b3616117c1a2168450861
rishikeshhulsure <89689516+rishikeshhulsure@users.noreply.github.com>
2024-10-06 16:03:47 +0530

    Update LaunchingBrowser.java

--- src/main/java/com/prac/auto/LaunchingBrowser.java
+++ src/main/java/com/prac/auto/LaunchingBrowser.java
@@ -7,5 +7,6 @@ public class LaunchingBrowser {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://google.com");
+		driver.findElement();
 	}
 }
